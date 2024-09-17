bb76b8adc293c5aca26418da7a4ddb353a86aaa4
Wulf <wulft@ucmail.uc.edu>
2024-09-17 13:25:55 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        boolean done = true;
+        String name = "Tom";
+        int age = 66;
+        double salary = 0;
+
+        System.out.println("Hello " + name);
+        System.out.println("You said your age was " + age);
+        System.out.println(done);
+    }
+}
